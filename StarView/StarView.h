5de09d01190aaf68d6684ee09b74d1a9bd6f2a53
white8818 <754578818@qq.com>
2016-04-14 11:01:03 +0800

    添加注释

--- StarView/StarView.h
+++ StarView/StarView.h
@@ -9,9 +9,17 @@
 #import <UIKit/UIKit.h>
 
 @interface StarView : UIView
-
+/**
+ *  初始化
+ *
+ *  @param frame 五角星的frame
+ *
+ *  @return return value description
+ */
 + (instancetype)starWithFrame:(CGRect)frame;
-
-@property (nonatomic, assign) CGFloat avgScore;//分值
+/**
+ *  百分比, 满星是1.0, 根据需求设置比例值
+ */
+@property (nonatomic, assign) CGFloat avgScore;
 
 @end
